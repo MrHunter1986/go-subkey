06bbe05ce414140df85d43b16ae9ac244fe3b8f3
root <root@ip-172-31-44-120.ap-southeast-1.compute.internal>
2022-02-09 14:44:01 +0800

    for mac gomobile

---
+++ cmd/subkey/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/vedhavyas/go-subkey"
+	"github.com/vedhavyas/go-subkey/sr25519"
+)
+
+func main() {
+	s := flag.String("secret", "", "Secret key in Hex")
+	m := flag.String("msg", "", "Message to be signed in Hex")
+	flag.Parse()
+
+	msg, ok := subkey.DecodeHex(*m)
+	if !ok {
+		panic(fmt.Errorf("invalid hex"))
+	}
+
+	kr, err := subkey.DeriveKeyPair(sr25519.Scheme{}, *s)
+	if err != nil {
+		panic(err)
+	}
+
+	sig, err := kr.Sign(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(kr.Verify(msg, sig))
+}
